5881041d6ddd6afe3a50dc5e84ab8f85b216b2d7
Julia Tucher <JuliaTucher@JuliaTucher2017insidecollege-preporg.local>
2018-10-30 16:06:26 -0400

    first

---
+++ q2/Library.fs
@@ -0,0 +1,19 @@
+module CS334
+
+type Tree<'a> =
+| Leaf of 'a
+| Node of Tree<'a> * Tree<'a>
+
+/// <summary> 
+/// The recursive function treduce takes a binary operation f and a tree t and combines all the leaves of t using the binary operation f.
+/// </summary>
+
+/// <param name= "f"> Function used to combine leaf values</param>
+/// <param name= "t"> Tree to be combines</param>
+
+/// <return> Returns the result of the combined tree of type output of f.</return>
+
+let rec treduce f t =
+    match t with
+    | Leaf (a) -> a
+    | Node (t1,t2) -> f (treduce f t1)(treduce f t2) 
