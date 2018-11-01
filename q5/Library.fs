21bc231f92e078e6cf8d5b95e8368e0d80f9a173
Julia Tucher <JuliaTucher@JuliaTucher2017insidecollege-preporg.local>
2018-10-31 23:05:36 -0400

    q5 done

--- q5/Library.fs
+++ q5/Library.fs
@@ -2,10 +2,15 @@ module CS334
 
 open Parser
 
+///<summary> The data type Expr is used to data for a valid expression in the Labda calculus grammar.</summary>///
 type Expr =
 | Variable of char
 | Abstraction of char * Expr
 | Application of Expr * Expr
+
+
+///<summary> The following parsers (expr, exprImpl, variable, abstraction, application, grammar) 
+///define parsers that correspond to the elements on the Lambda calculus grammar. </summary>///
 
 let expr, exprImpl = recparser()
 
@@ -19,11 +24,15 @@ exprImpl := variable <|> abstraction <|> application <!> "expr"
 
 let grammar = pleft expr peof <!> "top"
 
+
+///<summary> Parse is a function that takes in an input string 'input', prepares it, and returns an option of type Expr. </summary>///
 let parse input : Expr option =
     match grammar (prepare input) with
     | Success(res, _) -> Some res
     | Failure -> None
 
+
+///<summary> Prettyprint takes in an Expr 'e' and returns a string that represents given Lambda calculus expression. </summary>///
 let rec prettyprint(e: Expr) : string =
     match e with
     | Variable(c) -> "Variable(" + c.ToString() + ")"
--- q5/Program.fs
+++ q5/Program.fs
@@ -3,9 +3,15 @@ open CS334
 
 [<EntryPoint>]
 let main argv =
+    let printparse x = 
+        match x with
+        | Some x -> printfn "%A" (prettyprint x)
+        | None -> printfn "Invalid program."
     let asto = parse "((Lx.x)(Lx.y))"
-    match asto with
-    | Some ast -> printfn "%A" (prettyprint ast)
-    | None -> printfn "Invalid program."
+    printparse asto
+    let y = parse "(((Lx.((xx)(ax)))(Lx.(bx)))a)"
+    printparse y
+    let z = parse "(Lx.xx)(ay)"
+    printparse z
     0
 
